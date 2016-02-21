50158d26698fe8a7b22a9d670a9929dca0997aa0
Dr. Heinrich Hördegen <hoerdegen@energiefluss.info>
2016-02-21 12:14:46 +0100

    selections example

--- html/myD3Solutions/solutions.js
+++ html/myD3Solutions/solutions.js
@@ -3,6 +3,8 @@ require("./example1/script_v1.js");
 require("./example1/script_v2.js");
 require("./example1/script_v3.js");
 
+require("./selection/script.js");
+
 require("./example2/script_v1.js");
 require("./example2/script_v2.js");
 require("./example2/script_v3.js");
